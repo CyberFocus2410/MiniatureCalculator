a8f8c665fe3254de5af9f965c1de8e2a42f3a082
CyberFocus2410 <cyberfocus2410@gmail.com>
2025-07-17 00:24:26 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='MiniatureCalculator',
+    version='1.0',
+    description='Educational calculator & converter',
+    author='Vivan',
+    packages=[],
+    include_package_data=True,
+)
